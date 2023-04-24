b40462282b75c1b1391cf7432abb336a16bf7d20
evgenjnr <evgeniyr7@gmail.com>
2023-04-24 18:43:29 +0300

    feat(scss): move style.scss to style directory update project.json #10

--- apps/frontend/client/src/app/app.module.ts
+++ apps/frontend/client/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent],
@@ -11,6 +12,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
     BrowserAnimationsModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
